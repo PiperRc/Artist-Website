4494c5325e8606bd02d3a36b7699723966c11b56
Cheyenne Piper <robeltopiper@gmail.com>
2023-11-22 16:18:27 -0500

    Change success.astro to submit.astro

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded',()=>{
+  
+//     const currentUrl=window.location.href;
+// const  navLink=document.querySelectorAll('.nav-link');
+
+//     if(currentUrl){
+// navLink.classList.add('nav-active');
+//     }
+// })
