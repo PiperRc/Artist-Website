af9b224233677f2d112e0efbd495a3e205c34816
Cheyenne Piper <robeltopiper@gmail.com>
2023-11-24 00:54:31 -0500

    Add back to top button

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,19 @@
 // navLink.classList.add('nav-active');
 //     }
 // })
+
+document.addEventListener("DOMContentLoaded", function() {
+    let backToTopButton = document.querySelector(".top");
+
+    window.addEventListener("scroll", function() {
+        // Calculate the scroll position
+        let scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+        // Set the display property of the button based on the scroll position
+        if (scrollPosition > window.innerHeight* 7/8) {
+            backToTopButton.classList.add('visible')
+        } else {
+            backToTopButton.classList.remove('visible')
+        }
+    });
+});
